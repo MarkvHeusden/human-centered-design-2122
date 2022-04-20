bae839095d67a55d277a1789ef5aee72fe8fbd89
Mark <74104639+MarkvHeusden@users.noreply.github.com>
2022-04-20 11:31:46 +0200

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+
+const app = express()
+const port = process.env.PORT || 3333
+
+app.use(express.static('public'))
+// app.use(express.json())
+app.set('view engine', 'ejs')
+// app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port http://localhost:${port}`)
+})
